40d987768121ba9d4a9614c5cbebc4909c3e8b55
rs-kannan <rskannansrinivasan@gmail.com>
2023-09-06 19:03:49 +0530

    cookies in postman

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,18 @@ const sendToken = (user, statusCode, res) => {
   ///creating JWT Token
   const token = user.getJwtToken();
 
-  res.status(statusCode).json({
+  //setting cookies
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    secure: true,
+    httpOnly: true,
+  };
+
+  res.status(statusCode)
+  .cookie('token',token,options)
+  .json({
     success: true,
     token,
     user,
